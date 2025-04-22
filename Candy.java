51de38a131b0f652cc2ed65ac94c29086b3bbb35
thedoorknocks <nathanl60@nycstudents.net>
2025-04-22 02:13:39 +0000

    v2

--- Candy.java
+++ Candy.java
@@ -1,14 +1,56 @@
-public class Candy{
+public class Candy {
     private String flavor;
-    public Candy(String f){
+
+    public Candy(String f) {
         flavor = f;
     }
-    public BoxOfCandy(Candy[][] b)
-    {
-        box = b;
-    }
-    public String toString(){
+
+    public String toString() {
         return flavor;
     }
 
+    // BoxOfCandy inner class
+    public static class BoxOfCandy {
+        private Candy[][] box;
+
+        public BoxOfCandy(Candy[][] b) {
+            box = b;
+        }
+
+        public String toString() {
+            String s = "";
+            for (int row = 0; row < box.length; row++) {
+                for (int col = 0; col < box[0].length; col++) {
+                    s += box[row][col] + " ";
+                }
+                s += "\n";
+            }
+            return s;
+        }
+
+        public static boolean moveCandyToFirstRow(Candy[][] box, int col) {
+            if (box[0][col] != null) return true;  
+            for (int i = 1; i < box.length; i++) {
+                if (box[i][col] != null) {
+                    box[0][col] = box[i][col];  
+                    box[i][col] = null;
+                    return true;
+                }
+            }
+            return false; 
+        }
+
+        public Candy removeNextByFlavor(String flavor) {
+            for (int row = box.length - 1; row >= 0; row--) {
+                for (int col = 0; col < box[0].length; col++) {
+                    if (box[row][col] != null && box[row][col].flavor.equals(flavor)) {
+                        Candy removedCandy = box[row][col];
+                        box[row][col] = null;  
+                        return removedCandy; 
+                    }
+                }
+            }
+            return null;  
+        }
+    }
 }
--- Main.java
+++ Main.java
@@ -1,25 +1,36 @@
-public class Main{
-    public static void main(String[] args){
-        Candy[][] box = new Candy[3][4];
+public class Main {
+    public static void main(String[] args) {
+        Candy[][] box = new Candy[4][3];
         box[0][1] = new Candy("lime");
         box[1][1] = new Candy("orange");
         box[2][2] = new Candy("cherry");
         box[3][1] = new Candy("lemon");
         box[3][2] = new Candy("grape");
 
-        BoxOfCandy b = new BoxOfCandy(box);
+        Candy.BoxOfCandy b = new Candy.BoxOfCandy(box);
         System.out.println(b);
 
-        public String toString()
-        {
-            String s = "";
-            for(int row = 0; row<box.length; row++){
-                for(int col = 0; col <box[0].length; col++){
-                    s += box[row][col] + "";
-                }
-                s += "/n"
-            }
-            return s;
-        }
+
+        boolean result1 = Candy.BoxOfCandy.moveCandyToFirstRow(box, 0);
+        System.out.println(result1); 
+        boolean result2 = Candy.BoxOfCandy.moveCandyToFirstRow(box, 1);
+        System.out.println(result2); 
+        boolean result3 = Candy.BoxOfCandy.moveCandyToFirstRow(box, 1);
+        System.out.println(result3);
+        System.out.println("");
+
+        box = new Candy[3][5];
+        box[0][0] = new Candy("lime");
+        box[0][1] = new Candy("lime");
+        box[0][3] = new Candy("lemon");
+        box[1][0] = new Candy("orange");
+        box[1][3] = new Candy("lime");
+        box[1][4] = new Candy("lime");
+        box[2][0] = new Candy("cherry");
+        box[2][2] = new Candy("lemon");
+        box[0][4] = new Candy("orange");
+
+        System.out.println(b.removeNextByFlavor("cherry"));
+        System.out.println(b);
     }
 }
